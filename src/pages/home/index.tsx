4b857c3792f875a3453f761410aba35445b0d476
Nilton de Oliveira Diniz <nilton.diniz@radixeng.com.br>
2023-01-27 14:09:46 -0300

    Commit inicial

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,35 @@
+import { Container, Movie, MovieList } from "./components";
+import { useState, useEffect } from "react";
+import { API_KEY } from "../../config/api_key";
+
+
+export default function Home(){
+
+    const [movies, setMovies] = useState<any[]>([])
+    const image_path = "https://image.tmdb.org/t/p/w500"
+
+    useEffect(() => {
+        //Consumir a api
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt-BR&page=1`)
+            .then(response => response.json())
+            .then(data => setMovies(data.results))
+    }, [])
+
+    return (
+        <Container>
+            <h1>Movies</h1>
+            <MovieList>
+                {
+                    movies.map(movie => {
+                        return (
+                            <Movie>
+                                <a href="#"><img src={`${image_path}${movie.poster_path}`} alt={movie.title}/></a>
+                                <span>{movie.title}</span>
+                            </Movie>
+                        )
+                    })
+                }
+            </MovieList>
+        </Container>
+    );
+}
